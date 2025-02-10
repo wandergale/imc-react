2df650d92dd65beac1f9be9520c74f7a08b33e5c
Wanderson notebook <wandersonlife7@gmail.com>
2025-02-10 20:59:00 -0300

    adicionando logica

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,21 +1,42 @@
 import style from "./Form.module.css";
 
+import { useState } from "react";
+
 const Form = () => {
+  const [height, setHeight] = useState("");
+  const [weight, setWeight] = useState("");
+
   const handleSubmit = (e) => {
     e.preventDefault();
+
+    const heightNum = Number(height);
+    const weightNum = Number(weight);
+
+    const imc = weightNum / Math.pow(heightNum, 2);
+    console.log(imc.toFixed(2));
   };
 
   return (
     <form onSubmit={handleSubmit} className={style.formContainer}>
       <label>
         Altura
-        <input type="text" name="altura" placeholder="Sua altura em cm..." />
+        <input
+          type="text"
+          name="altura"
+          placeholder="Sua altura em cm..."
+          onChange={(e) => setHeight(e.target.value)}
+        />
       </label>
       <label>
         Peso
-        <input type="text" name="peso" placeholder="Seu peso em kg's" />
+        <input
+          type="text"
+          name="peso"
+          placeholder="Seu peso em kg's"
+          onChange={(e) => setWeight(e.target.value)}
+        />
       </label>
-      <input type="button" value="Calcular" className={style.btnForm} />
+      <input type="submit" value="Calcular" className={style.btnForm} />
     </form>
   );
 };
